cc02801f645852280894f2b30a4ca92bc10d7992
spieljs <spieljs@gmail.com>
2020-11-21 23:57:23 +0100

    add Celery provider example

---
+++ README.md
@@ -0,0 +1,39 @@
+# Celery consumer
+
+This is an example of provider with [Rust Celery](https://rusty-celery.github.io/) and [Warp](https://github.com/seanmonstar/warp) which will get the complete result task from [Sled](https://sled.rs/) database.
+
+# how it works
+
+1. Install [Rabbitmq](https://www.rabbitmq.com/)
+
+2. Clone the project 
+
+3. Inside of the project directory add the .env file with the path of you Sled database (notice that it will be sharing with the consumer):
+
+```
+SLED_URL=[YOUR SLED PATH]
+AMPQ_ADDR=[YOUR AMPQ URL]
+```
+
+4. Execute `cargo run`
+
+5. Run the [Consumer](git@github.com:dancespiele/celery_consumer.git) example to see all the complete flow
+
+6. Execute this curl
+
+```
+curl --location --request GET 'http://localhost:3000/hello' \
+--header 'Content-Type: application/json'
+```
+
+## Do you like it?
+
+If you liked, please help me to make more different examples in rust with:
+
+- BAT rewards in case that you use [Brave Browser](https://brave.com/)
+- [Github Sponsors](https://github.com/sponsors/dancespiele)
+- Burst coins to the address BURST-DPN6-2AT3-FCRL-9BBKG
+
+## License
+
+Celery consumer is [Apache-2.0](LICENSE.md) licensed
---
+++ src/main.rs
@@ -0,0 +1,95 @@
+extern crate celery;
+extern crate structopt;
+use celery::{TaskResult, Celery, broker::AMQPBroker};
+use celery::error::{CeleryError};
+use std::net::SocketAddr;
+use warp::{filters::BoxedFilter, Filter};
+use warp::{reply, Rejection, Reply, reject};
+use std::{error::Error};
+use sled::{Db, IVec, Error as DbError};
+use std::str;
+use dotenv::dotenv;
+use std::env;
+use tokio::time::{delay_for, Duration};
+
+#[celery::task]
+fn send_text(text: String) -> TaskResult<String> {
+    Ok(text)
+}
+
+fn init_my_app() -> Result<BoxedFilter<(&'static Celery<AMQPBroker>,)>, Box<dyn Error>> {
+    let my_app = celery::app!(
+        broker = AMQP { std::env::var("AMPQ_ADDR").unwrap_or_else(|_| "amqp://127.0.0.1:5672".into())},
+        tasks = [
+            send_text,
+        ],
+        task_routes = [
+            "send_text" => "text_queue",
+        ]
+    );
+
+    Ok(warp::any().map(move || my_app.clone()).boxed())
+}
+
+pub fn init_tree() -> Result<Db, DbError> {
+    let seld_url = env::var("SLED_URL").expect("SLED_URL must be set");
+    sled::open(seld_url)
+}
+
+async fn send_hello(my_app: &'static Celery<AMQPBroker>) -> Result<impl Reply, Rejection> {
+    let response: IVec;
+    let mut index = 0;
+    let id_message =             my_app.send_task(send_text::new("hello consumer".to_string())).await
+    .or_else(|_| Err(CeleryError::TaskRegistrationError(String::from("unknown task")))).unwrap();
+
+    delay_for(Duration::from_millis(100)).await;
+
+    loop {
+        let db_result = init_tree();
+
+        if let Ok(db) = db_result {
+            let response_result = db.get(id_message.clone());
+
+            if let Ok(response_option) = response_result {
+                if let Some(res) = response_option {
+                    response = res;
+                    break;
+                }
+            }
+        }
+
+        index += 1;
+        
+        if index > 10 {
+            return Err(reject::not_found());
+        }
+
+        delay_for(Duration::from_millis(100)).await;
+    }
+
+    let message_body = str::from_utf8(&response).unwrap();
+
+    Ok(reply::json(&message_body))
+}
+
+fn hello_filter(
+    my_app: impl Filter<Extract = (&'static Celery<AMQPBroker>,), Error = Rejection> + Clone + Send,
+) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    warp::path("hello")
+        .and(warp::get())
+        .and(my_app)
+        .and_then(send_hello)
+}
+
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let my_app = init_my_app().unwrap();
+
+    let routes = hello_filter(my_app);
+
+    warp::serve(routes).run(addr).await;
+}
